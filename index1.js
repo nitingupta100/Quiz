a6b28fa797b978e357f68faf7ccc88d592d7c92c
nitingupta100 <nitin100@gmail.com>
2023-05-29 23:05:53 +0530

    web page

--- index1.js
+++ index1.js
@@ -1,4 +1,43 @@
-var readConsole = require('readline-sync')
-console.log("This is quiz on capital cities")
-var answer = readConsole.question("what is your name")
-console.log(answer)
+function check(e){
+    if (e.srcElement.id===b1){
+
+        var ans = document.querySelector('#a1')
+
+        if (ans.value ==='london'){
+            alert("correct")
+        }
+        else {
+             alert('wrong')
+        }
+
+    }
+
+    if (e.srcElement.id===b2){
+
+        var ans = document.querySelector('#a2')
+
+        if (ans.value ==='athens'){
+            alert("correct")
+        }
+        else {
+             alert('wrong')
+        }
+
+    }
+
+    if (e.srcElement.id===b3){
+
+        var ans = document.querySelector('#a3')
+
+        if (ans.value ==='bangkok'){
+            alert("correct")
+        }
+        else {
+             alert('wrong')
+        }
+
+    }
+    
+    
+}
+

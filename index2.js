83d92908ef8e6a19eafa915ffe1a360df7ece26c
nitingupta100 <nitin100@gmail.com>
2023-05-30 15:23:32 +0530

    quiz2 - completed

--- index2.js
+++ index2.js
@@ -7,22 +7,77 @@ var q2 = {
     q: "What is the capital of England ? ",
     a:"London"
 }
-
 var q3 ={
     q: "What is the capital of Thailand ? ",
     a: "Bangkok"
 }
 
 var questions = [q1,q2,q3]
-
 function buildQuiz(questions){
 
     var quiz = document.querySelector("#quiz-questions")
 
-    for (var i = 0; i < questions.length; i++){
-        quiz.innerHTML = quiz.innerHTML + "<div>" + questions[i].q + "<br> </br>" + "</div>"
-    }
+        //quiz.innerHTML = <input type ="text"/>
 
+    // input = document.createElement('input')
+    // input.innerHTML = "type='textbox"
+    // console.log(input)
+
+    for (var i = 0; i < questions.length; i++){
+        
+        var j = i + 1
+        quiz.innerHTML = quiz.innerHTML + "<div>" + questions[i].q + "<input id='a"+ j +"'" + "type='text'>" + "<button id='b" + j + "' onclick='check(event)'" + ">Check</button>"  + "</div>"
+        console.log(quiz.innerHTML)
+    }
 }
 
 buildQuiz(questions)
+
+function check(e){
+
+    if (e.srcElement.id==='b1'){
+        console.log(e.srcElement.id)
+
+        var ans = document.getElementById('a1')
+        console.log(ans)
+        console.log(ans.value)
+        console.log(questions[0].a)
+
+        if (ans.value === questions[0].a){
+            alert("correct")
+        }
+        else {
+             alert('wrong')
+        }
+
+    }
+
+    if (e.srcElement.id==='b2'){
+
+        var ans = document.querySelector('#a2')
+
+        if (ans.value ===questions.q2.a){
+            alert("correct")
+        }
+        else {
+             alert('wrong')
+        }
+
+    }
+
+    if (e.srcElement.id==='b3'){
+
+        var ans = document.querySelector('#a3')
+
+        if (ans.value ===questions.q3.a){
+            alert("correct")
+        }
+        else {
+             alert('wrong')
+        }
+
+    }
+    
+    
+}
+
